bddb930c1f226da67dc6f434f1e4aadd86202661
wadakenji <ponporopen@gmail.com>
2022-06-17 22:08:24 +0900

    style: .eslintrc.js コード整形

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
+  env: { browser: true, es2021: true },
   extends: ['airbnb-base'],
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   plugins: ['@typescript-eslint'],
   rules: {
     semi: 'off',
