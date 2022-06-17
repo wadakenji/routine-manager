ef5dd30c219f2b768f7130e2adb359a27dbed593
wadakenji <ponporopen@gmail.com>
2022-06-17 20:48:22 +0900

    feat: emoji用テキストの定数追加

--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -23,3 +23,24 @@ export const RECORD_ITEM_TEXT_JA: { [k in keyof DailyRecordInSheet]: string } = 
   meditation: '瞑想',
   stretch: 'ストレッチ',
 }
+
+/** emoji用テキスト */
+export const EMOJI: Record<string, `:${string}:`> = {
+  SUNNY: ':sunny:',
+  CLOUDY: ':cloud:',
+  RAINY: ':umbrella_with_rain_drops:',
+  SUNNY_CLOUDY: ':partly_sunny:',
+  CLOUDY_RAINY: ':rain_cloud:',
+  SUNNY_CLOUDY_RAINY: ':partly_sunny_rain:',
+  SUN_WITH_FACE: ':sun_with_face:',
+  MOON_WITH_FACE: ':full_moon_with_face:',
+  WALKING: ':man-walking:',
+  FIRE: ':fire:',
+  RUNNING: ':man-running:',
+  TRAINING: ':man-lifting-weights:',
+  SEAL: ':seal:',
+  MEDITATION: ':man_in_lotus_position:',
+  BATHING: ':bath:',
+  THUMBS_UP: ':thumbsup:',
+  SOB: ':sob:',
+}
