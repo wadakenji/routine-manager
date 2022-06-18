ea2b1946553bb3e834d5f4f2d2ec5d600be96bc6
wadakenji <ponporopen@gmail.com>
2022-06-18 13:37:12 +0900

    feat: 不要になった変数を削除

--- src/configs/constant.ts
+++ src/configs/constant.ts
@@ -5,25 +5,6 @@ export const ROUTINE_SPREADSHEET_ID =
 /** ポストするSlackのwebhook URL */
 export const SLACK_HOOK_URL = PropertiesService.getScriptProperties().getProperty('SLACK_HOOK_URL') || ''
 
-/** 各項目名の日本語テキスト */
-export const RECORD_ITEM_TEXT_JA: { [k in keyof DailyRecordInSheet]: string } = {
-  date: '日付',
-  weather: '天気',
-  temperature: '気温',
-  getUpTime: '起床',
-  goToBedTime: '就寝',
-  feeling: '気分',
-  awakening: '寝起き',
-  weight: '体重',
-  fatPercentage: '体脂肪率',
-  steps: '歩数',
-  running: 'ランニング',
-  training: '筋トレ',
-  hiit: 'HIIT',
-  meditation: '瞑想',
-  stretch: 'ストレッチ',
-}
-
 /** emoji用テキスト */
 export const EMOJI = {
   SUNNY: ':sunny:',
