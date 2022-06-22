b97b0f9c864ef389563fdc3922cef0fe94ed9852
wadakenji <ponporopen@gmail.com>
2022-06-22 18:25:38 +0900

    feat: 筋トレ記録に関するmain関数追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { getRecord, updateRecord } from './libs/spreadsheet/routine'
 import { recordJsonToInput, recordToText } from './libs/convert'
 import { postSlackMessage } from './libs/slack'
+import { addExerciseRow, addTrainingRecordRow, getAllExercises, isExistingExercise } from './libs/spreadsheet/training'
 
 // gas-webpack-pluginの使い方として、gasで叩ける関数はglobalオブジェクトに紐付ける
 // cf. https://github.com/fossamagna/gas-webpack-plugin
@@ -29,3 +30,24 @@ global.update = (record: DailyRecordInJson, dateValue: string | number | Date = 
   const input = recordJsonToInput(record)
   updateRecord(date, input)
 }
+
+/**
+ * 筋トレ記録を追加する
+ *
+ * @param part - 体の部位
+ * @param exercise - 種目
+ * @param weight - 重量
+ * @param rep - 回数（レップ数）
+ * @return { exists: boolean } - 種目が既に存在していたかどうかを返す
+ */
+global.addTrainingRecord = (part: string, exercise: string, weight: number, rep: number) => {
+  const exerciseExists = isExistingExercise(part, exercise)
+  if (!exerciseExists) addExerciseRow(part, exercise)
+  addTrainingRecordRow(new Date(), exercise, weight, rep)
+  return { exerciseExists }
+}
+
+/**
+ * 筋トレの種目データを返す
+ */
+global.getExercises = () => getAllExercises()
